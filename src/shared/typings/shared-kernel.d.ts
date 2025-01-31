f07dd3933ccdb1244521bcc5c3cc55bf4a05b496
Skippia <pocketbook.love24@gmail.com>
2025-02-01 00:50:59 +0300

    refactor: fp 8

--- src/shared/typings/shared-kernel.d.ts
+++ src/shared/typings/shared-kernel.d.ts
@@ -27,3 +27,13 @@ declare type TTask<T> = () => Promise<T>
 declare type AsyncFunction<T = unknown, R = unknown> = (arg: T) => Promise<R>
 declare type SyncFunction<T = unknown, R = unknown> = (arg: T) => R
 declare type AnyFunction<T = unknown, R = unknown> = AsyncFunction<T, R> | SyncFunction<T, R>
+
+declare type ReadonlyDeep<T> = Readonly<{
+  [K in keyof T]:
+  // Is it a primitive? Then make it readonly
+  T[K] extends (number | string | symbol) ? Readonly<T[K]>
+    // Is it an array of items? Then make the array readonly and the item as well
+    : T[K] extends Array<infer A> ? Readonly<Array<DeepReadonly<A>>>
+    // It is some other object, make it readonly as well
+      : DeepReadonly<T[K]>;
+}>
